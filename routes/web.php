df691832ac06756decd666725fa71fc926b6481a
hona97 <phuochoan1999qng@gmail.com>
2022-10-27 17:45:59 +0700

    user update

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Payment\AddPaymentController;
 
 /*
 |--------------------------------------------------------------------------
